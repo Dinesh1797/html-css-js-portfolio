f4dec9885905f074cdeae4260b9f16a6162b4971
Dinesh Kumar A <dineshayyadurai97@gmail.com>
2023-08-01 00:59:01 +0530

    Finished website

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links"); //Targeting Element
+    const icon = document.querySelector(".hamburger-icon"); //Targeting Element
+    menu.classList.toggle("open") //Targeting Element
+    icon.classList.toggle("open") //Targeting Element
+}
